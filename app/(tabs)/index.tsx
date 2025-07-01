aa26f139447a8266b18b4d68a1c39aacd011ee00
Aleksander Grygierczyk <me@aleksander.cc>
2025-06-30 17:59:23 -0700

    experiment

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,51 @@
-import { StyleSheet } from 'react-native';
-
+import { ScrollView, StyleSheet } from 'react-native';
+import * as Device from 'expo-device';
+import { useState, useEffect } from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+export function InfoCard({ title, value }: { title: string; value: string }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={styles.infoCard}>
+      <Text style={styles.infoCardTitle}>{title}</Text>
+      <Text style={styles.infoCardValue}>{value}</Text>
     </View>
+  );
+}
+
+export default function TabOneScreen() {
+  const [uptime, setUptime] = useState(0);
+  const [rooted, setRooted] = useState(false);
+  const [deviceType, setDeviceType] = useState<Device.DeviceType | null>(null);
+  useEffect(() => {
+    Device.getUptimeAsync().then(setUptime);
+    Device.isRootedExperimentalAsync().then(setRooted);
+    Device.getDeviceTypeAsync().then(setDeviceType);
+  }, []);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <InfoCard title="Uptime" value={`${Math.round(uptime / 1000 / 60 / 60 / 24)} days`} />
+      <InfoCard title="Brand" value={Device.brand ?? 'Unknown'} />
+      <InfoCard title="Model" value={Device.modelName ?? 'Unknown'} />
+      <InfoCard title="OS" value={Device.osBuildId ?? 'Unknown'} />
+      <InfoCard title="OS Version" value={Device.osVersion ?? 'Unknown'} />
+      <InfoCard title="Manufacturer" value={Device.manufacturer ?? 'Unknown'} />
+      <InfoCard title="Rooted" value={rooted ? 'Yes' : 'No'} />
+      <InfoCard title="Device Type" value={deviceType?.toString() ?? 'Unknown'} />
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 20,
+  },
+  scrollView: {
+    width: '100%',
   },
   title: {
     fontSize: 20,
@@ -27,5 +55,19 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  infoCard: {
+    backgroundColor: '#222222',
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 20,
+    width: '90%',
+  },
+  infoCardTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  infoCardValue: {
+    fontSize: 14,
   },
 });
